0462a468c09e173091d2533bb83c0fa2a1548a46
mrbeesley <mrbeesley@gmail.com>
2018-07-09 19:31:28 -0500

    first committ of the base models

---
+++ DataModelBase/Definitions/ModelStates.cs
@@ -0,0 +1,10 @@
+﻿namespace DataModelBase.Definitions
+{
+    public enum ModelStates
+    {
+        NoChange = 0,
+        New = 1,
+        Updated = 2,
+        Deleted = 3
+    }
+}
---
+++ DataModelBase/Interfaces/IModel.cs
@@ -0,0 +1,18 @@
+﻿using DataModelBase.Definitions;
+
+namespace DataModelBase.Interfaces
+{
+    public interface IModel
+    {
+        /// <summary>
+        /// The Id that will be used to reference the object in a datastore
+        /// </summary>
+        string Id { get; set; }
+
+        /// <summary>
+        /// The current state of the model
+        /// </summary>
+        ModelStates State { get; }
+
+    }
+}
