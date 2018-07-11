a1de95c124243b7ec8a2520f1ae6a5090d3c626e
mrbeesley <mrbeesley@gmail.com>
2018-07-10 19:59:22 -0500

    added an interface for the data service

---
+++ DataModelBase/Interfaces/IModelDBService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModelBase.Interfaces
+{
+    public interface IModelDBService<T> where T : IModel
+    {
+        Task<List<T>> GetData();
+
+        Task<T> GetDataById(string id);
+
+        Task<T> SaveData(T model);
+
+        Task<bool> DeleteData(string id);
+    }
+}
