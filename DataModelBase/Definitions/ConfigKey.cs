04c9dbccc18cade4d12c2437c0afb6850d7908bc
mrbeesley <mrbeesley@gmail.com>
2018-07-27 08:31:50 -0500

    updated interfaces

---
+++ DataModelBase/Definitions/ConfigKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModelBase.Definitions
+{
+    public enum ConfigKey
+    {
+        Id,
+        Database,
+        Collection
+    }
+}
---
+++ DataModelBase/Interfaces/IDbConnector.cs
@@ -0,0 +1,17 @@
+﻿using DataModelBase.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModelBase.Interfaces
+{
+    public interface IDbConnector
+    {
+        Task<List<T>> GetData<T>(Dictionary<string, string> config) where T : IModel;
+
+        Task<T> SaveData<T>(T item, Dictionary<string, string> config) where T : IModel;
+
+        Task<bool> DeleteData(Dictionary<string, string> config);
+    }
+}
